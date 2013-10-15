f519a8bd2fc055dbbaa93d59143ee8152c74679a
Victor Pikula <victor@pikula.com>
2013-10-15 08:58:22 +0000

    User login is now mandatory site-wide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate_user!
   before_filter :load_settings, :set_default_mailer_host
   after_filter :store_location
 
